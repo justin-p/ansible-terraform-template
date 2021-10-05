a525888fc5b068906a02cfb8ab02786187378250
Justin Perdok <justin-p@users.noreply.github.com>
2021-10-05 11:59:31 +0200

    Update to how hostnames work (#3)

--- terraform/digitalocean/vm/main.tf
+++ terraform/digitalocean/vm/main.tf
@@ -1,3 +1,9 @@
+resource "random_pet" "name" {
+  count = var.module_enabled ? 1 : 0 # only run if this variable is true
+  
+  length = 2
+}
+
 resource "random_string" "name" {
   count = var.module_enabled ? 1 : 0 # only run if this variable is true
     
@@ -6,12 +12,17 @@ resource "random_string" "name" {
   upper   = false
 }
 
+locals {
+  # if server_hostname is null (which is the default value), generate a random hostname with random_pet and random_string
+  server_hostname = var.server_hostname != null ? var.server_hostname : "${random_pet.name[0].id}-${random_string.name[0].result}" # [0] selector is required due the `count = var.module_enabled` trick.
+}
+
 resource "digitalocean_droplet" "main" {
   count = var.module_enabled ? 1 : 0 # only run if this variable is true
 
+  name               = local.server_hostname
   image              = var.server_image
   tags               = flatten(var.server_tags)
-  name               = "${var.project_name}-${var.server_name}-${terraform.workspace}-${random_string.name[0].result}"
   region             = var.server_region
   size               = var.server_size
   ipv6               = var.server_ipv6
@@ -20,13 +31,13 @@ resource "digitalocean_droplet" "main" {
   ssh_keys           = var.server_ssh_keys
 }
 
-resource "null_resource" "ssh_check" {   # Ensure that SSH is ready and accepting connections on all hosts.
+resource "null_resource" "ssh_check" {   # ensure that SSH is ready and accepting connections on all hosts.
   count = var.module_enabled ? 1 : 0 # only run if this variable is true
 
   connection {
     type        = "ssh"
     user        = var.root_username
-    host        = digitalocean_droplet.main[0].ipv4_address
+    host        = digitalocean_droplet.main[0].ipv4_address # [0] selector is required due the `count = var.module_enabled` trick.
     private_key = file("${var.root_ssh_key_path}")
   }
 
